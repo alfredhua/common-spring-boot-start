3601f480922e289703e36c908983162155811bde
hua_zhenguo <hua_zhenguo@163.com>
2020-02-25 16:52:56 +0800

    修改redis

--- src/main/java/com/common/redis/RedisConfig.java
+++ src/main/java/com/common/redis/RedisConfig.java
@@ -2,32 +2,23 @@ package com.common.redis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
+@ConditionalOnClass(RedisUtils.class)
+@ConditionalOnProperty(prefix = "spring.redis", value = "enable", matchIfMissing = true)
 public class RedisConfig {
-
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-//        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-//        //开启事务
-//        redisTemplate.setEnableTransactionSupport(true);
-//        redisTemplate.setConnectionFactory(redisConnectionFactory);
-//        return redisTemplate;
-//    }
 
     @Bean
     public RedisUtils redisUtils(@Autowired RedisConnectionFactory connectionFactory,
-                                                  @Autowired ObjectMapper objectMapper){
+                                 @Autowired ObjectMapper objectMapper){
         return new RedisUtils(
-            RedisOptions.createValueOptions(connectionFactory,objectMapper,Object.class),
-            RedisOptions.createValueOptions(connectionFactory,objectMapper,String.class),
-            RedisOptions.createListOptions(connectionFactory));
+                RedisOptions.createValueOptions(connectionFactory,objectMapper,Object.class),
+                RedisOptions.createValueOptions(connectionFactory,objectMapper,String.class),
+                RedisOptions.createListOptions(connectionFactory));
     }
 }
