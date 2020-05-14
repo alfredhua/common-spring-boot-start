90cc70786dc00f8fdfb6b60a2143ff2786eade62
hua_zhenguo <hua_zhenguo@163.com>
2020-05-14 17:19:48 +0800

    redis

--- src/main/java/com/common/redis/RedisUtils.java
+++ src/main/java/com/common/redis/RedisUtils.java
@@ -6,6 +6,7 @@ import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class RedisUtils {
@@ -31,6 +32,24 @@ public class RedisUtils {
       template.expire(key, timeout, TimeUnit.SECONDS);
     }
   }
+
+  /**
+   * 从redis中随机返回一个key
+   * @return
+   */
+  public Object randomKey() {
+    return template.randomKey();
+  }
+
+  /**
+   * 返回 key 所储存的值的类型
+   * @param key
+   * @return
+   */
+  public DataType getType(String key) {
+    return template.type(key);
+  }
+
 
   /**
    * 根据key 获取过期时间
@@ -69,7 +88,7 @@ public class RedisUtils {
    * @param timeout 过期时间，0：表示使用默认30天,null：表示永不过期
    * @param value
    */
-  public void set(String key,Long timeout,Object value){
+  public void objectSet(String key,Long timeout,Object value){
     try {
       if (timeout==null){
         template.opsForValue().set(key, value);
@@ -87,7 +106,7 @@ public class RedisUtils {
    * @param key
    * @return
    */
-  public Object get(String key){
+  public Object objectGet(String key){
     try {
       return template.opsForValue().get(key);
     }catch ( Exception e){
@@ -127,7 +146,7 @@ public class RedisUtils {
    * @param value 值
    */
   public void hashSet(String key, String field, Object value) {
-      template.opsForHash().put(key, field, value);
+    template.opsForHash().put(key, field, value);
   }
 
   /**
@@ -160,25 +179,296 @@ public class RedisUtils {
 
 
 
-
-
-
   /**
-   * 从redis中随机返回一个key
-   * @return
+   * 判断key中的hashKey是否存在
+   * @param key
+   * @param field
    */
-  public Object randomKey() {
-    return template.randomKey();
+  public Boolean hasKey(String key,String field) {
+    return template.opsForHash().hasKey(key, field);
   }
 
   /**
-   * 返回 key 所储存的值的类型
+   * list left push
+   * @param key
+   * @param value
+   */
+  public void listLeftPush(String key,Object value) {
+    template.opsForList().leftPush(key, value);
+  }
+
+  /**
+   * list left pop
    * @param key
    * @return
    */
-  public DataType getType(String key) {
-    return template.type(key);
+  public Object listLeftPop(String key) {
+    return template.opsForList().leftPop(key);
   }
+
+
+
+  /**
+   * list right push
+   * @param key
+   * @param value
+   */
+  public void listRightPush(String key,Object value) {
+    template.opsForList().rightPush(key, value);
+  }
+
+  /**
+   * list right pop
+   * @param key
+   * @return
+   */
+  public Object listRightPop(String key) {
+    return template.opsForList().rightPop(key);
+  }
+
+
+  /**
+   * 获取该key index处的元素
+   * @param key
+   * @param index
+   * @return
+   */
+  public Object listGetIndex(String key,int index) {
+    return template.opsForList().index(key, index);
+  }
+
+  /**
+   * 获取列表的长度
+   * @param key
+   * @return
+   */
+  public Long listSize(String key) {
+    return template.opsForList().size(key);
+  }
+
+
+  /**
+   * 获取key中下标从start到end处的值
+   * @param key
+   * @param start 开始下标
+   * @param end  结束下标
+   * @return
+   */
+  public List<Object> listRange(String key,int start,int end){
+    return template.opsForList().range(key, start, end);
+  }
+
+  /**
+   * 删除
+   * @param key
+   * @return
+   */
+  public Long listRemove(String key,long count,Object value){
+    return template.opsForList().remove(key, count, value);
+  }
+
+  /**
+   * 向set 集合中添加
+   * @param key
+   * @param values
+   */
+  public void setAdd(String key,Object...values) {
+    template.opsForSet().add(key, values);
+  }
+
+  /**
+   * 移除并取出第一个元素
+   * @param key
+   * @return
+   */
+  public Object setGet(String key){
+    return template.opsForSet().pop(key);
+  }
+
+  /**
+   * 返回集合中所有的元素
+   * @param key
+   * @return
+   */
+  public Set<Object> setGetAll(String key){
+    return template.opsForSet().members(key);
+  }
+
+
+  /**
+   * 返回指定数量的元素(随机)
+   * @param key
+   * @param count
+   * @return
+   */
+  public List<Object> setRandomMembers(String key,int count){
+    return template.opsForSet().randomMembers(key, count);
+  }
+
+  /**
+   * 返回集合中的长度
+   * @param key
+   * @return
+   */
+  public Long setGetSize(String key){
+    return template.opsForSet().size(key);
+  }
+
+  /**
+   * 返回给定集合的差集（返回 key在otherKeys不存在的元素）
+   * @param key 主集合
+   * @param otherKeys 其他集合
+   * @return
+   */
+  public Set<Object> setDifference(String key,String...otherKeys){
+    return template.opsForSet().difference(key, CollectionUtils.arrayToList(otherKeys));
+  }
+
+  /**
+   * 返回给定集合的交集（返回 key与otherKeys中共同存在的元素）
+   * @param key
+   * @param otherKeys
+   * @return
+   */
+  public Set<Object> setIntersect(String key,String...otherKeys){
+    return template.opsForSet().intersect(key, CollectionUtils.arrayToList(otherKeys));
+  }
+  /**
+   * 返回给定集合的并集（key和otherKeys加起来的所有元素，共同拥有的元素只返回一个）
+   * @param key
+   * @param otherKeys
+   * @return
+   */
+  public Set<Object> setUnion(String key,String...otherKeys){
+    return template.opsForSet().union(key, CollectionUtils.arrayToList(otherKeys));
+  }
+
+  /**
+   * 返回集合中的所有元素
+   * @param key
+   * @return
+   */
+  public Set<Object> setMembers(String key){
+    return template.opsForSet().members(key);
+  }
+
+  /**
+   * 迭代集合中的元素
+   * @param key
+   * @return
+   */
+  public Cursor<Object> setScan(String key){
+    return template.opsForSet().scan(key, ScanOptions.NONE);
+  }
+
+  //zSet操作 有序集合
+
+  /**
+   * 添加数据
+   *
+   * 添加方式：
+   * 1.创建一个set集合
+   * Set<ZSetOperations.TypedTuple<Object>> sets=new HashSet<>();
+   * @param key
+   * @param tuples
+   */
+  public void zSetAdd(String key,Set<ZSetOperations.TypedTuple<Object>> tuples) {
+    template.opsForZSet().add(key, tuples);
+  }
+
+  /**
+   * 获取有序集合的成员数
+   * @param key
+   * @return
+   */
+  public Long zSetCard(String key) {
+    return template.opsForZSet().zCard(key);
+  }
+
+  /**
+   * 计算在有序集合中指定区间分数的成员数
+   * @param key
+   * @param min 最小排序分数
+   * @param max 最大排序分数
+   * @return
+   */
+  public Long zSetCount(String key,Double min,Double max) {
+    return template.opsForZSet().count(key, min, max);
+  }
+
+  /**
+   * 获取有序集合下标区间 start 至 end 的成员  分数值从小到大排列
+   * @param key
+   * @param start
+   * @param end
+   */
+  public Set<Object> zSetRange(String key,int start,int end) {
+    return template.opsForZSet().range(key, start, end);
+  }
+
+  /**
+   * 获取有序集合下标区间 start 至 end 的成员  分数值从大到小排列
+   * @param key
+   * @param start
+   * @param end
+   */
+  public Set<Object> zSetReverseRange(String key,int start,int end) {
+    return template.opsForZSet().reverseRange(key, start, end);
+  }
+
+  /**
+   * 返回 分数在min至max之间的数据 按分数值递减(从大到小)的次序排列。
+   * @param key
+   * @param min
+   * @param max
+   * @return
+   */
+  public Set<Object> zSetReverseRange(String key,Double min,Double max) {
+    return template.opsForZSet().reverseRangeByScore(key, min, max);
+  }
+
+  /**
+   * 返回指定成员的下标
+   * @param key
+   * @param value
+   * @return
+   */
+  public Long zSetRank(String key,Object value) {
+    return template.opsForZSet().rank(key, value);
+  }
+
+  /**
+   * 删除key的指定元素
+   * @param key
+   * @param values
+   * @return
+   */
+  public Long zSetRemoveValue(String key,Object values) {
+    return template.opsForZSet().remove( key, values);
+  }
+
+  /**
+   * 移除下标从start至end的元素
+   * @param key
+   * @param start
+   * @param end
+   * @return
+   */
+  public Long zSetRemoveRange(String key,int start,int end) {
+    return template.opsForZSet().removeRange(key, start, end);
+  }
+
+  /**
+   * 移除分数从min至max的元素
+   * @param key
+   * @param min
+   * @param max
+   * @return
+   */
+  public Long zSetRemoveRangeByScore(String key,Double min,Double max) {
+    return template.opsForZSet().removeRangeByScore(key, min, max);
+  }
+
 
 
 
