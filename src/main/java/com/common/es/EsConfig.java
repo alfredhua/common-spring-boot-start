931e6e8d16d3d8efd08c9d471271ac6e23c5f353
guozhenhua <guozhenhua7@jd.com>
2020-08-28 15:49:03 +0800

    transportclient

--- src/main/java/com/common/es/EsConfig.java
+++ src/main/java/com/common/es/EsConfig.java
@@ -1,10 +1,50 @@
 package com.common.es;
 
+import lombok.Data;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
+import java.io.IOException;
+import java.net.InetAddress;
+
+@Data
 @Configuration
-//@ConditionalOnClass(RedisUtils.class)
+@ConditionalOnClass(EsTransportClient.class)
 @ConditionalOnProperty(prefix = "es", value = "enable", matchIfMissing = true)
 public class EsConfig {
+
+    public String clusterName;
+
+    public String ips;
+
+    public boolean cluster;
+
+    @Bean
+    @ConditionalOnProperty(value = "es")
+    public EsTransportClient createClient() throws IOException {
+        TransportClient client=null;
+        if (cluster){
+            //集群模式，配置机器名称
+            Settings settings= Settings.builder().put("cluster.name",clusterName).put("client.transport.sniff", false).build();
+            client = new PreBuiltTransportClient(settings);
+        }else{
+            //非集群运行
+            client = new PreBuiltTransportClient(Settings.EMPTY);
+        }
+        String[] ipPorts = ips.split(",");
+        for (String ipPort : ipPorts) {
+            String[] ipPortArray = StringUtils.split(ipPort, ":");
+            client.addTransportAddress(
+                    new InetSocketTransportAddress(InetAddress.getByName(ipPortArray[0]), Integer.parseInt(ipPortArray[1])));
+        }
+        return new EsTransportClient(client);
+    }
+
 }
--- src/main/java/com/common/es/EsTransportClient.java
+++ src/main/java/com/common/es/EsTransportClient.java
@@ -1,17 +1,36 @@
 package com.common.es;
 
+import com.common.util.GsonUtils;
+import lombok.Data;
+import org.elasticsearch.client.Client;
+
 /**
  * EsTransportClient简介
  * TransportClient客户端
  * @author guozhenhua
  * @date 2020-08-28 14:46
  */
-public class EsTransportClient {
 
-    /**
-     * 初始化方法
-     */
-    public void init() {
+@Data
+public class EsTransportClient<V> {
 
+    private Client client;
+
+    private String indexName;
+
+    private String typeName;
+
+    public EsTransportClient(Client client) {
+        this.client = client;
     }
+//
+//    public void serIndexAndType(String indexName,String typeName){
+//        indexName.set(indexName);
+//        typeName.set(typeName);
+//    }
+
+    public void saveIndex(V indexBean, String id) throws Exception {
+        client.prepareIndex(indexName, typeName, id).setSource(GsonUtils.toJSON(indexBean)).setRouting(id).execute().actionGet();
+    }
+
 }
